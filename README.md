8eb169b134b4c5a5245f5128dd5b04612d3b86fc
mail <you@example.com>
2017-09-26 12:51:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Highgain (GAIN)
+
+
+
+
+
+ALGO: Qubit
+Block Size: 8 MB
+Min. Stake Age: 3 Hours
+Max. Stake Age: Unlimited
+COINBASE MATURITY: 25
+RPCPORT:4665
+PORT:4664
+
+PoW:
+82 Millions Airdrop Distribution
+FPoS:
+9% High Gain Interest
+PoS start block: 500
+
+
+
---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x00000a998d828917e78819561b7fe7fe043a0ccc7972bbfa481c6fb21a72b145");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
